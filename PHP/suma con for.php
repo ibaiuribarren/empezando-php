a290a89606fe8f7cf1982f62b9256caaee09e1df
AS3-2-21 <AS3-2-21@uni.lan>
2019-10-08 10:21:34 +0200

    php

---
+++ PHP/suma con for.php
@@ -0,0 +1,33 @@
+
+
+<?php
+$v = [1,5,6];
+
+
+//for
+$suma = 0;
+for ($i = 0; $i < 6; $i++) {
+    //echo "The number is: $x <br>";
+    $suma = $suma + $v[$i];
+      echo $suma;
+      echo "<br>";
+}
+echo " batura : " . $suma;
+
+//while
+<?php
+$x = 0;
+
+while($x <= 6) {
+    echo "The number is: $x <br>";
+    $x++;
+}
+?>
+
+
+
+
+//foreach
+}
+
+?>
